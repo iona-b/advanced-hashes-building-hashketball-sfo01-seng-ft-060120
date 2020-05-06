eff128b92c55523cf5850f99a663411894dea11b
Iona <ionabrabender@gmail.com>
2020-05-06 01:31:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,8 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [{
+      :players => [
+      {
         :player_name => "Alan Anderson",
         :number => 0,
         :shoe => 16,
@@ -62,7 +63,8 @@ def game_hash
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
-      :players => [{
+      :players => [
+      {
         :player_name => "Jeff Adrien",
         :number => 4,
         :shoe => 18,
